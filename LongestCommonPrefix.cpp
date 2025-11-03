9ce6520c4579b8ffbe2fb51eada13ade7d10796d
Jenil Gohel <jgohel9902@gmail.com>
2025-11-02 22:11:11 -0500

    Longest Common Prefix
    Added problem description and level for clarity.

---
+++ LongestCommonPrefix.cpp
@@ -0,0 +1,25 @@
+// Problem 14 : Longest Common Prefix
+// LEVEL : Easy
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty strin
+
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size() - 1);
+                if (prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
